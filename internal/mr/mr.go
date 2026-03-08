a607bf6eb2cc39ffd5e6584a2ed680664e8bdb11
Gogo <g.t.kostov@student.tue.nl>
2026-03-09 00:42:08 +0100

    Apply paths from feat/new-stuff

--- internal/mr/mr.go
+++ internal/mr/mr.go
@@ -6,15 +6,14 @@ import (
 	"git-split/internal/provider"
 )
 
-func Create(repo provider.Repo, title, base, head string) error {
-
+func Create(repo provider.Repo, title, description, base, head string) error {
 	switch repo.Provider {
 	case provider.GitHub:
-		return provider.CreateGitHubPR(repo, title, base, head)
+		return provider.CreateGitHubPR(repo, title, description, base, head)
 	case provider.GitLab:
-		return provider.CreateGitLabMR(repo, title, base, head)
+		return provider.CreateGitLabMR(repo, title, description, base, head)
 	case provider.GitBucket:
-		return provider.CreateGitBucketPR(repo, title, base, head)
+		return provider.CreateGitBucketPR(repo, title, description, base, head)
 	}
 	return fmt.Errorf("unknown provider")
 }
