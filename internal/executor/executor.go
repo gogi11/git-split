e64168dd33c6e0ff637d0b229b915f757595557f
Gogo <g.t.kostov@student.tue.nl>
2026-03-08 23:52:41 +0100

    fix: add proper logging

--- internal/executor/executor.go
+++ internal/executor/executor.go
@@ -13,27 +13,36 @@ import (
 func Execute(p plan.Plan) error {
 	remote, err := git.GetRemoteURL()
 	if err != nil {
+		log.Fatal(err)
 		return err
 	}
+	fmt.Printf("Remote URL: %s\n", remote)
 	repoInfo, err := provider.ParseRemote(remote)
 	if err != nil {
+		log.Fatal(err)
 		return err
 	}
+	fmt.Printf("Executing plan for repository: %s\n", repoInfo)
 
 	for _, branch := range p.Branches {
 		err = git.Checkout(branch.Base)
 		if err != nil {
+			log.Fatal(err)
 			return err
 		}
+		fmt.Printf("Checked out to branch: %s\n", branch.Base)
 		err := git.CreateBranch(branch.Base, branch.Branch)
 		if err != nil {
+			log.Fatal(err)
 			return err
 		}
+		fmt.Printf("Created branch: %s\n", branch.Branch)
 		for _, op := range branch.Operations {
 			switch op.Type {
 			case plan.OpCherryPick:
 				err := git.CherryPickCommits(op.Commits)
 				if err != nil {
+					log.Fatal(err)
 					return err
 				}
 			case plan.OpApplyPath:
@@ -43,12 +52,15 @@ func Execute(p plan.Plan) error {
 						path,
 					)
 					if err != nil {
+						log.Fatal(err)
 						return err
 					}
 				}
+				fmt.Printf("Applied these files: %s\n", op.Paths)
 				msg := fmt.Sprintf("Apply paths from %s", op.FromRef)
 				err := git.Commit(msg)
 				if err != nil {
+					log.Fatal(err)
 					return err
 				}
 			}
@@ -58,6 +70,7 @@ func Execute(p plan.Plan) error {
 			if err != nil {
 				log.Fatal(err)
 			}
+			fmt.Printf("Pushed branch: %s\n", branch.Branch)
 			if branch.CreateMR {
 				err := mr.Create(
 					repoInfo,
@@ -67,9 +80,11 @@ func Execute(p plan.Plan) error {
 					branch.Branch,
 				)
 				if err != nil {
+					log.Fatal(err)
 					return err
 				}
 			}
+			fmt.Printf("Created MR for branch: %s\n", branch.Branch)
 		}
 	}
 	return nil
