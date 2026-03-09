fc813db7b96aa34272ae4a40e2b046bef03a0f44
Gogo <g.t.kostov@student.tue.nl>
2026-03-09 02:12:34 +0100

    feat/new-stuff: Split 2

--- internal/plan/plan.go
+++ internal/plan/plan.go
@@ -1,12 +1,29 @@
 package plan
 
+import "git-split/internal/git"
+
+type OperationType string
+
+const (
+	OpCherryPick OperationType = "cherry-pick"
+	OpApplyPath  OperationType = "apply-path"
+)
+
+type Operation struct {
+	Type        OperationType
+	Commits     []string
+	FileChanges []git.FileChange
+	FromRef     string
+}
+
 type BranchPlan struct {
-	Branch   string
-	Base     string
-	Commits  []string
-	Push     bool
-	CreateMR bool
-	MRTitle  string
+	Branch        string
+	Base          string
+	Operations    []Operation
+	Push          bool
+	CreateMR      bool
+	MRTitle       string
+	MRDescription string
 }
 
 type Plan struct {
